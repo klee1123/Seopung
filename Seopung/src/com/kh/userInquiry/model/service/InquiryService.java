67361efdd6e6d6506a936eab1e705e9ba2d6b43f
RIA AN <mojicoretro@gmail.com>
2020-10-03 21:36:57 +0900

    1:1문의 값 넘기기 수정중

--- Seopung/src/com/kh/userInquiry/model/service/InquiryService.java
+++ Seopung/src/com/kh/userInquiry/model/service/InquiryService.java
@@ -1,17 +1,21 @@
 package com.kh.userInquiry.model.service;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.kh.userInquiry.model.dao.InquiryDao;
+import com.kh.userInquiry.model.vo.Inquiry;
 
 public class InquiryService {
 
 	/*
 	 * 1:1문의 등록용
 	 */
-	
 	public int insertInquiry() {
 		Connection conn = getConnection();
 		
-		int result = new InquiryDao().insertInquiry(conn);
+		int result = new InquiryDao().insertInquiry(conn, 
+					inquiryType, userEmail, userPrivacy);
 		
 		if(result>0) {
 			commit(conn);
